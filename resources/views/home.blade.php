bc5d3320118f614c66724c27c9cca713988e3a40
rybalkoserg <rybalkoserg@ukr.net>
2016-06-20 10:03:36 +0300

    006 Controllers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,9 @@
 
 @section('content')
     <div class="centered">
-        <a href="{{ route('greet') }}">Greet</a>
-        <a href="{{ route('hug') }}">Hug</a>
-        <a href="{{ route('kiss') }}">Kiss</a>
+        <a href="{{ route('niceaction', ['action' => 'greet']) }}">Greet</a>
+        <a href="{{ route('niceaction', ['action' => 'hug']) }}">Hug</a>
+        <a href="{{ route('niceaction', ['action' => 'kiss']) }}">Kiss</a>
         <br/>
         <br/>
         <form action="{{ route('benice') }}" method="post">
