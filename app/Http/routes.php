7d06f1c38b370aee2fd738e3c4a07f8021f63908
rybalkoserg <rybalkoserg@ukr.net>
2016-06-20 09:32:39 +0300

    005 Grouping Routes together

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,25 +15,29 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/greet/{name?}', function ($name = null) {
-    return view('actions.greet', ['name' => $name]);
-})->name('greet');
+Route::group(['prefix' => 'do'], function(){
+    Route::get('/greet/{name?}', function ($name = null) {
+        return view('actions.greet', ['name' => $name]);
+    })->name('greet');
 
-Route::get('/hug', function () {
-    return view('actions.hug');
-})->name('hug');
+    Route::get('/hug', function () {
+        return view('actions.hug');
+    })->name('hug');
 
-Route::get('/kiss', function () {
-    return view('actions.kiss');
-})->name('kiss');
+    Route::get('/kiss', function () {
+        return view('actions.kiss');
+    })->name('kiss');
 
-Route::post('/benice', function(\Illuminate\Http\Request $request){
-    if(isset($request['action']) && $request['name']){
-        if(strlen($request['name']) > 0){
-            return view('actions.nice', ['action' => $request['action'], 'name' => $request['name']]);
+    Route::post('/benice', function(\Illuminate\Http\Request $request){
+        if(isset($request['action']) && $request['name']){
+            if(strlen($request['name']) > 0){
+                return view('actions.nice', ['action' => $request['action'], 'name' => $request['name']]);
+            }
+            return redirect()->back();
         }
         return redirect()->back();
-    }
-    return redirect()->back();
-})->name('benice');
+    })->name('benice');
+});
+
+
 
